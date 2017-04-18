7bf18e34836a7c3b137ae3f2453ccbf3f41c49cc
بهنام محمدی <itten@live.com>
2017-04-18 13:59:49 +0430

    commit add Array.toLocaleString()

--- array.polyfill.js
+++ array.polyfill.js
@@ -987,3 +987,107 @@ if (!Array.prototype.some) {
     return false;
   };
 }
+
+/** 
+ * Array.sort()
+ * version 0.0.0
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	1       1       5.5	                (Yes)   (Yes)   (Yes)
+ * -------------------------------------------------------------------------------
+ */
+
+/** 
+ * Array.splice()
+ * version 0.0.0
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	1       1       5.5	                (Yes)   (Yes)   (Yes)
+ * -------------------------------------------------------------------------------
+ */
+
+/** 
+ * Array.toLocaleString()
+ * version 0.0.0
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	(Yes)   (Yes)   (Yes) 	            (Yes)   (Yes)   (Yes)
+ * -------------------------------------------------------------------------------
+ */
+if (!Array.prototype.toLocaleString) {
+  Object.defineProperty(Array.prototype, 'toLocaleString', {
+    value: function(locales, options) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var a = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(A, "length")).
+      var len = a.length >>> 0;
+
+      // 3. Let separator be the String value for the 
+      //    list-separator String appropriate for the 
+      //    host environment's current locale (this is 
+      //    derived in an implementation-defined way).
+      // NOTE: In this case, we will use a comma
+      var separator = ',';
+
+      // 4. If len is zero, return the empty String.
+      if (len === 0) {
+        return '';
+      }
+
+      // 5. Let firstElement be ? Get(A, "0").
+      var firstElement = a[0];
+      // 6. If firstElement is undefined or null, then
+      //  a.Let R be the empty String.
+      // 7. Else,
+      //  a. Let R be ? 
+      //     ToString(? 
+      //       Invoke(
+      //        firstElement, 
+      //        "toLocaleString", 
+      //        « locales, options »
+      //       )
+      //     )
+      var r = firstElement == null ? 
+        '' : firstElement.toLocaleString(locales, options);
+
+      // 8. Let k be 1.
+      var k = 1;
+
+      // 9. Repeat, while k < len
+      while (k < len) {
+        // a. Let S be a String value produced by 
+        //   concatenating R and separator.
+        var s = r + separator;
+
+        // b. Let nextElement be ? Get(A, ToString(k)).
+        var nextElement = a[k];
+
+        // c. If nextElement is undefined or null, then
+        //   i. Let R be the empty String.
+        // d. Else,
+        //   i. Let R be ? 
+        //     ToString(? 
+        //       Invoke(
+        //        nextElement, 
+        //        "toLocaleString", 
+        //        « locales, options »
+        //       )
+        //     )
+        r = nextElement == null ? 
+          '' : nextElement.toLocaleString(locales, options);
+
+        // e. Let R be a String value produced by 
+        //   concatenating S and R.
+        r = s + r;
+
+        // f. Increase k by 1.
+        k++;
+      }
+
+      // 10. Return R.
+      return r;
+    }
+  });
+}
