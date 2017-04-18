cec1fd29bc40ca5a102da5cce6600e3660f7baa8
بهنام محمدی <itten@live.com>
2017-04-18 13:54:35 +0430

    commit add Array.reduceRight()

--- array.polyfill.js
+++ array.polyfill.js
@@ -893,3 +893,40 @@ if (!Array.prototype.reduce) {
     }
   });
 }
+
+/** 
+ * Array.reduceRight()
+ * version 0.0.0
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	(Yes)   3       9	                10.5    4       ?
+ * -------------------------------------------------------------------------------
+ */
+if ('function' !== typeof Array.prototype.reduceRight) {
+  Array.prototype.reduceRight = function(callback /*, initialValue*/) {
+    'use strict';
+    if (null === this || 'undefined' === typeof this) {
+      throw new TypeError('Array.prototype.reduce called on null or undefined');
+    }
+    if ('function' !== typeof callback) {
+      throw new TypeError(callback + ' is not a function');
+    }
+    var t = Object(this), len = t.length >>> 0, k = len - 1, value;
+    if (arguments.length >= 2) {
+      value = arguments[1];
+    } else {
+      while (k >= 0 && !(k in t)) {
+        k--;
+      }
+      if (k < 0) {
+        throw new TypeError('Reduce of empty array with no initial value');
+      }
+      value = t[k--];
+    }
+    for (; k >= 0; k--) {
+      if (k in t) {
+        value = callback(value, t[k], k, t);
+      }
+    }
+    return value;
+  };
+}
