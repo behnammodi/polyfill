94072a0c4b42b7299ba9ad4c7dd9884746292cf2
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2017-12-27 10:15:59 +0330

    fixed bug in padEnd and padStart

--- string.polyfill.js
+++ string.polyfill.js
@@ -252,18 +252,18 @@ if (!String.prototype.normalize) {
  * -------------------------------------------------------------------------------
  */
 if (!String.prototype.padEnd) {
-    String.prototype.padEnd = function padEnd(targetLength,padString) {
-        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-        padString = String(padString || ' ');
+    String.prototype.padEnd = function padEnd(targetLength, padString) {
+        targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
         if (this.length > targetLength) {
             return String(this);
         }
         else {
-            targetLength = targetLength-this.length;
+            targetLength = targetLength - this.length;
             if (targetLength > padString.length) {
-                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+                padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
             }
-            return String(this) + padString.slice(0,targetLength);
+            return String(this) + padString.slice(0, targetLength);
         }
     };
 }
@@ -276,18 +276,18 @@ if (!String.prototype.padEnd) {
  * -------------------------------------------------------------------------------
  */
 if (!String.prototype.padStart) {
-    String.prototype.padStart = function padStart(targetLength,padString) {
-        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-        padString = String(padString || ' ');
+    String.prototype.padStart = function padStart(targetLength, padString) {
+        targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
         if (this.length > targetLength) {
             return String(this);
         }
         else {
-            targetLength = targetLength-this.length;
+            targetLength = targetLength - this.length;
             if (targetLength > padString.length) {
-                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+                padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
             }
-            return padString.slice(0,targetLength) + String(this);
+            return padString.slice(0, targetLength) + String(this);
         }
     };
 }
