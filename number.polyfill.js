79cfaabf1043bc37a100fac5b15a0f70c91f050d
Behnam Mohammadi <itten@live.com>
2020-10-26 14:07:55 +0330

    add Number.isInteger polyfill

---
+++ number.polyfill.js
@@ -0,0 +1,16 @@
+/**
+ * Number.isInteger()
+ * version 0.0.0
+ * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
+ * Basic support	  34  	  16      (No)	              21	  9       12
+ * -------------------------------------------------------------------------------
+ */
+if (!Number.isInteger) {
+  Number.isInteger = function (value) {
+    return (
+      typeof value === 'number' &&
+      isFinite(value) &&
+      Math.floor(value) === value
+    );
+  };
+}
