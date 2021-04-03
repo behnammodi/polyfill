6170d3d19e28b5a15b8f689708156e6874326efa
Behnam Mohammadi <itten@live.com>
2021-04-04 01:21:39 +0430

    style(function): apply prettier

--- function.polyfill.js
+++ function.polyfill.js
@@ -9,20 +9,18 @@
  * -------------------------------------------------------------------------------
  */
 
-
 /**
  * Function.prototype.name
  * Feature	        Chrome  Firefox Internet Explorer   Opera	Safari	Edge
  * Basic support	  15      1       No                  10.5  6       14
  * ------------------------------------------------------------------------
  */
-
-if( Function.prototype.name!=="" ) {
+if (Function.prototype.name !== "") {
   var rx = /function\ ([\w$]+)\(/;
   Object.defineProperty(Function.prototype, "name", {
-    get: function() {
-      var match = rx.exec(this+"");
-      return match && match.length===2 ? match[1] : "";
+    get: function () {
+      var match = rx.exec(this + "");
+      return match && match.length === 2 ? match[1] : "";
     },
     configurable: true,
   });
