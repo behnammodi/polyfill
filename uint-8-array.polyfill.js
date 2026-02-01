5e68e5b2f6f74475fc92c25434ec2e617297963c
Behnam <behnam.modi@outlook.com>
2026-02-01 09:06:31 +0000

    fix(int8, uint8): cleanup

--- uint-8-array.polyfill.js
+++ uint-8-array.polyfill.js
@@ -5,21 +5,15 @@
  * Basic support    No      No      No                  No    No      No
  * -------------------------------------------------------------------------------
  */
- if (!Uint8Array.prototype.at) {
-  Object.defineProperty(Uint8Array.prototype, "at",
-    {
-      value: function (n) {
-        // ToInteger() abstract op
-        n = Math.trunc(n) || 0;
-        // Allow negative indexing from the end
-        if (n < 0) n += this.length;
-        // OOB access is guaranteed to return undefined
-        if (n < 0 || n >= this.length) return undefined;
-        // Otherwise, this is just normal property access
-        return this[n];
-      },
-      writable: true,
-      enumerable: false,
-      configurable: true
-    });
+if (!Uint8Array.prototype.at) {
+  Uint8Array.prototype.at = function (n) {
+    // ToInteger() abstract op
+    n = Math.trunc(n) || 0;
+    // Allow negative indexing from the end
+    if (n < 0) n += this.length;
+    // OOB access is guaranteed to return undefined
+    if (n < 0 || n >= this.length) return undefined;
+    // Otherwise, this is just normal property access
+    return this[n];
+  }
 }
